a0d332ccb0bbd393b5b00990f23912ccd69ee39c
xintao <phil77@126.com>
2026-03-09 14:16:07 +0800

    fix

--- cs336_alignment/sft.py
+++ cs336_alignment/sft.py
@@ -74,7 +74,7 @@ def run_sft(
     )
 
     model_path = str(model_path)
-    llm = init_vllm(model_path, eval_device, seed)
+    llm = init_vllm(model_path, eval_device, seed, gpu_memory_utilization=0.7)
     eval_prompt_template = load_prompt_template(prompt_path)
     eval_prompts = format_prompts(eval_data, eval_prompt_template)
 
