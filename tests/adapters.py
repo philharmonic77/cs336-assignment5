dd1457c0f828c772f98887ecacbddeff7cb3be80
xintao <phil77@126.com>
2026-03-10 12:19:03 +0800

    masked_normalize

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 from cs336_alignment.sft import tokenize_prompt_and_output, compute_entropy, get_response_log_probs,\
-    sft_microbatch_train_step
+    sft_microbatch_train_step, masked_normalize
 
 
 def run_tokenize_prompt_and_output(
@@ -269,7 +269,7 @@ def run_masked_normalize(
         torch.Tensor, the normalized sum, where masked elements
             (mask=0) don't contribute to the sum.
     """
-    raise NotImplementedError
+    return masked_normalize(tensor, mask, normalize_constant, dim)
 
 
 """
